4e5eedb6487cf6c0e59d87772f868097cd750c8e
arturand <44437751+arturand@users.noreply.github.com>
2019-12-15 23:27:21 +0200

    Add files via upload
    First task

---
+++ tests/unit/IteminList.spec.js
@@ -0,0 +1,21 @@
+// Import the mount() method from the test utils
+// and the component you want to test
+import { mount } from '@vue/test-utils'
+import List from "../../src/components/List.vue";
+import Item from "../../src/models/Item"
+
+describe('ItemsinList',() => {
+    let list = []
+
+
+    const wrapper = mount(List, {
+        propsData: {
+            list:list
+        }
+    });
+    it('Empty list add first ToDo', () => {
+        expect(wrapper.find('tere').text()).toEqual('Add your first Todo task');
+
+    })
+});
+
