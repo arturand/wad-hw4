61940dce237ebb9ab92f2d7a3073095865f8b22f
siimanderson <andersonsiim95@gmail.com>
2019-12-15 23:21:52 +0200

    list test done

--- tests/unit/List.spec.js
+++ tests/unit/List.spec.js
@@ -2,8 +2,23 @@
 // and the component you want to test
 import { mount } from '@vue/test-utils'
 import List from "../../src/components/List.vue";
+import Item from "../../src/models/Item.js";
 
 describe( 'List is rendered correctly', () =>{
     // Now mount the component and you have the wrapper
-    const wrapper = mount(List);
+    let list = [];
+
+    let test = new Item("new");
+    list.push(test);
+
+    const wrapper = mount(List, {
+      propsData: {
+        list: list
+      }
+    });
+    
+    it('When an item in the list is marked as done item is updated correctly ', () => {
+      wrapper.find('span').trigger('click');
+      expect(list[0].done).toEqual(true);
+    })
 });
